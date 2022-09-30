d5ad240c0b0b6c0f373b4da511ee1995e7f1c1a4
igor <igor.pravdin163@gmail.com>
2022-09-30 22:32:36 +0400

    2

---
+++ 2/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите целое число M");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите целое число N, которое больше М");
+int n = int.Parse(Console.ReadLine()!);
+
+int count = 0;
+int SumSegment(int first, int second, int count2)
+{
+    if (first == second) return count2 + first;
+    else return SumSegment(first + 1, second, count2 += first);
+}
+
+Console.WriteLine($"Сумма натуральный элементов на прмежутке от {m} до {n} = {SumSegment(m, n, count)}");
